2d503598f3a80ffc3c54be7c748bb220c8a8e0fa
bever <bever@beverdeimac-9.local>
2016-02-23 04:02:19 +0800

    Initial Commit

---
+++ ButtonModel/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ButtonModel
+//
+//  Created by bever on 16/2/23.
+//  Copyright © 2016年 bever. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ButtonModel/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ButtonModel
+//
+//  Created by bever on 16/2/23.
+//  Copyright © 2016年 bever. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
